40c14566dcf369661052dbccfc79ef55df04f62e
Rafaela0Cruz <rafaelap.cruz0@gmail.com>
2019-05-29 18:17:15 -0300

    CameraFollow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject player;
+    public float lerpRate = 4f;
 
-    // Update is called once per frame
+    bool follow = true;
+
+
+
     void Update()
     {
-        
+        if (follow == false){
+            return;
+        }
+
+        if (player.transform.position.y > transform.position.y) {
+            Vector3 initialPosition = transform.position;
+            Vector3 finalPosition = new Vector3(initialPosition.x, player.transform.position.y, initialPosition.z);
+            transform.position = Vector3.Lerp(initialPosition, finalPosition, lerpRate* Time.deltaTime);
+        }
     }
+
+
+     public void TurnOff() {
+            follow = false;
+        }
 }
